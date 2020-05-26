bf7167101c12da92daae5ff9cbcf0387d3cd3797
贾元发 <jiayuanfa@yeah.net>
2020-05-26 15:35:57 +0800

    Text路径效果

---
+++ app/src/main/java/com/example/motionlayoutdemo/TextMotionActivity.kt
@@ -0,0 +1,18 @@
+package com.example.motionlayoutdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout.DEBUG_SHOW_PATH
+import kotlinx.android.synthetic.main.activity_text_motion.*
+
+class TextMotionActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_motion)
+
+        // 显示DEBUG路径
+        motionLayout.setDebugMode(DEBUG_SHOW_PATH)
+
+    }
+}
